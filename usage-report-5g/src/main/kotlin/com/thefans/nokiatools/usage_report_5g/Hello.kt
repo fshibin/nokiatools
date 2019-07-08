302c89947708d04b1585480baf6129e05444a917
fshibin <shibin.fan@gmail.com>
2019-07-08 23:21:53 +1200

    First commit

---
+++ usage-report-5g/src/main/kotlin/com/thefans/nokiatools/usage_report_5g/Hello.kt
@@ -0,0 +1,6 @@
+package com.thefans.nokiatools.usage_report_5g
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ usage-report-5g/src/test/kotlin/com/thefans/nokiatools/usage_report_5g/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.thefans.nokiatools.usage_report_5g
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
